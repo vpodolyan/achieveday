0268de1c9d926a1223bd2b882a6197055babe2ce
Vladimir Podolyan <vpodolyan@outlook.com>
2017-05-02 18:50:39 +0300

    Yarn support & updated dependencies

--- src/components/Achievement.js
+++ src/components/Achievement.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react'
+import PropTypes from 'prop-types';
+import React from 'react';
 
 const Achievement = ({id, text, onAchivDelete}) => (
     <li>
--- src/components/DatePicker.js
+++ src/components/DatePicker.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import PropTypes from 'prop-types';
+import React from 'react';
 import { localeUtils } from '../utils/i18n'
 
 import DayPicker from 'react-day-picker'
