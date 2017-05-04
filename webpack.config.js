254e85955debe5300c6d1c6bcdca028b33fe5d7a
Vladimir Podolyan <vpodolyan@outlook.com>
2017-05-04 11:46:36 +0300

    Hot browser reloading is enabled

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,8 @@ var HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
   entry: [
-    './src/index'
+    './src/index',
+    'webpack-hot-middleware/client'
   ],
   output: {
     path: path.join(__dirname, 'build'),
