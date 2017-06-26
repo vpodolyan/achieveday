983cf99cae79609084c3549b71234a30d6871b8f
Vladimir Podolyan <vpodolyan@outlook.com>
2017-06-26 18:23:03 +0300

    Achievements list as a styled component

--- src/components/AchieveList.js
+++ src/components/AchieveList.js
@@ -1,25 +1,31 @@
-import PropTypes from 'prop-types';
 import React from 'react';
-import Achievement from './Achievement'
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+import Achievement from './Achievement';
+
+const List = styled.ul`
+  list-style-type: none;
+`;
 
 const AchieveList = ({ achievements, onAchivDelete }) => (
-  <ul>
-    {achievements.map(achievmnt =>
-      <Achievement
-        key={achievmnt.id}
-        {...achievmnt}
-        onAchivDelete={onAchivDelete}
-      />
-    )}
-  </ul>
+    <List>
+        {achievements.map(achievmnt =>
+            <Achievement
+                key={achievmnt.id}
+                {...achievmnt}
+                onAchivDelete={onAchivDelete}
+            />
+        )}
+    </List>
 )
 
 AchieveList.propTypes = {
-  achievements: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    text: PropTypes.string.isRequired
-  }).isRequired).isRequired,
-  onAchivDelete: PropTypes.func.isRequired
+    achievements: PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        text: PropTypes.string.isRequired
+    }).isRequired).isRequired,
+    onAchivDelete: PropTypes.func.isRequired
 }
 
 export default AchieveList
