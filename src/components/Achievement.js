0aae5b5e634bf05617dc69d36a226f291e9c96ea
Vladimir Podolyan <vpodolyan@outlook.com>
2017-06-26 18:23:03 +0300

    Achievements list as a styled component

--- src/components/Achievement.js
+++ src/components/Achievement.js
@@ -2,14 +2,16 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 const Achievement = ({id, text, onAchivDelete}) => (
-    <li>
+    <li className="lead">
         <span>{text}</span>
         <a href='#' onClick={() => onAchivDelete(id)}>delete</a>
     </li>
 )
 
 Achievement.PropTypes = {
-    text: PropTypes.string.isRequired
+    id: PropTypes.number.isRequired,
+    text: PropTypes.string.isRequired,
+    onAchivDelete: PropTypes.func
 }
 
 export default Achievement
