36f9eee50b227916422042b2785aeb5236a548fa
Vladimir Podolyan <vpodolyan@outlook.com>
2018-11-16 13:47:39 +0300

    Sample of using AuthService in the app

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,46 @@
 import React from 'react'
+import { Stitch } from 'mongodb-stitch-browser-sdk';
+
+import StichAuthService from '../services/auth/StitchAuthService';
+import { OAuthProviders } from '../services/auth/AuthService';
 
 import DayAchievements from '../containers/DayAchievements'
 import NewAchievement from '../containers/NewAchievement'
 import DatePickerContainer from '../containers/DatePickerContainer'
 
+const appId = 'achievedayapp-zjent';
+const client = Stitch.initializeDefaultAppClient(appId);
+
+const authService = new StichAuthService(client);
+
+if(client.auth.hasRedirectResult()){
+  client.auth.handleRedirectResult().then(user => {
+     console.log('USER ', user.profile.data);
+  });
+}
+
 const App = () => (
   <div>
+    {
+      !authService.isAuthenticated() && (
+        <div>
+          NOT LOGGED IN
+          <button onClick={() => authService.logInOauth(OAuthProviders.Google)}>
+            LogIn
+          </button>
+        </div>
+      )
+    }
+    {
+      authService.isAuthenticated() && (
+        <div>
+          LOGGED IN
+          <button onClick={() => authService.logOut()}>
+            LogOut
+          </button>
+        </div>
+      )
+    }
     <DatePickerContainer />
     <DayAchievements />
     <NewAchievement />
