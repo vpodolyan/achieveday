07db64b8157909697c50f94d16260b4f36936090
Vladimir Podolyan <vpodolyan@outlook.com>
2020-03-15 22:57:37 +0300

    Fix LoginPageContainer layout

--- src/components/pages/LoginPage/LoginPageContainer.tsx
+++ src/components/pages/LoginPage/LoginPageContainer.tsx
@@ -1,11 +1,15 @@
 import styled from 'styled-components';
 
 const LoginPageContainer = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    height: 100vh;
     background: #63C8F8;
     font-family: "Open Sans", "Ubuntu", "Helvetica Neue", sans-serif;
 `;
