eaf14477f576dd29894b8f98b24228cefea5718d
Vladimir Podolyan <vpodolyan@outlook.com>
2020-05-10 18:14:05 +0300

    Add IUser interface

--- src/types/state/IAppState.ts
+++ src/types/state/IAppState.ts
@@ -1,9 +1,11 @@
 import IAchievementsState from "./IAchievementsState";
 import IDatePickerState from "./IDatePickerState";
 import IQuotesState from "./IQuotesState";
+import IUser from "types/IUser";
 
 export default interface IAppState {
     achievements: IAchievementsState;
     datePicker: IDatePickerState;
     quotes: IQuotesState;
+    user: IUser;
 }
