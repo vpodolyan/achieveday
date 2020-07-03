32d19bda977f966406c0831d573ad5aa33801aba
Vladimir Podolyan <vpodolyan@outlook.com>
2020-07-03 23:54:56 +0300

    Add achievemens list item icon

--- src/components/Achievement.tsx
+++ src/components/Achievement.tsx
@@ -1,4 +1,12 @@
 import * as React from 'react';
+import styled from 'styled-components';
+
+import {faTrophy} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+
+const TrophyIcon = styled(FontAwesomeIcon)`
+    font-size: 1rem;
+`;
 
 interface IProps {
     id: object;
@@ -7,8 +15,9 @@ interface IProps {
 }
 
 const Achievement: React.FC<IProps> = ({ id, text, onAchivDelete }) => (
-    <div className="lead d-flex">
-        <div className=''>{text}</div>
+    <div className="lead d-flex align-items-center">
+        <TrophyIcon icon={faTrophy} />
+        <div className='pl-1'>{text}</div>
         <div className='col-2'>
             <a className='ml-1' href='#' onClick={() => onAchivDelete(id)}>delete</a>
         </div>
