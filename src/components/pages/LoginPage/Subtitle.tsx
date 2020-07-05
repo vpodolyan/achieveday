9f44fe28d5fd5eab2e4486bd8d290cc60693aac2
Vladimir Podolyan <vpodolyan@outlook.com>
2020-07-05 19:22:06 +0300

    Fix login page layout on mobile

--- src/components/pages/LoginPage/Subtitle.tsx
+++ src/components/pages/LoginPage/Subtitle.tsx
@@ -1,10 +1,18 @@
 import styled from 'styled-components';
 
 const SubTitle = styled.div`
-    font-size: 32px;
+    font-size: 2rem;
     text-align: center;
     color: floralwhite;
     margin-top: 8px;
+
+    @media (max-width: 1024px) {
+        font-size: 1.6rem;
+    }
+
+    @media (max-width: 320px) {
+        font-size: 1.4rem;
+    }
 `;
 
 export default SubTitle;
