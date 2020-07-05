1a129bba93ba04c2784d0795e76e386c71c942e1
Vladimir Podolyan <vpodolyan@outlook.com>
2020-07-05 18:47:09 +0300

    Fix spinner layout

--- src/components/AchieveList.tsx
+++ src/components/AchieveList.tsx
@@ -25,18 +25,23 @@ const AchieveList: React.FC<IProps> = ({ achievements, onAchivDelete, getAchieve
 
     return (
       <div className="d-flex justify-content-center">
-        <List className="p-0 pt-4 pt-md-5 d-flex-column col-sm-12 col-md-8">
-          {loading && <Spinner size={2} />}
-          {achievements &&
-            achievements.map((achievement) => (
-              <Achievement
-                key={achievement._id?.toString()}
-                id={achievement._id}
-                text={achievement.text}
-                onAchivDelete={onAchivDelete}
-              />
-            ))}
-        </List>
+        {loading ? (
+          <div className='pt-5'>
+            <Spinner size={2} />
+          </div>
+        ) : (
+          <List className="p-0 pt-4 pt-md-5 d-flex-column col-sm-12 col-md-8">
+            {achievements &&
+              achievements.map((achievement) => (
+                <Achievement
+                  key={achievement._id?.toString()}
+                  id={achievement._id}
+                  text={achievement.text}
+                  onAchivDelete={onAchivDelete}
+                />
+              ))}
+          </List>
+        )}
       </div>
     );
 }
