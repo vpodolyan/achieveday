811e713bdc4777540d05712c6e8bf4554ac07907
Vladimir Podolyan <vpodolyan@outlook.com>
2020-07-20 19:38:48 +0300

    Replace delete btn with cross icon

--- src/components/Achievement.tsx
+++ src/components/Achievement.tsx
@@ -1,32 +1,44 @@
-import * as React from 'react';
-import styled from 'styled-components';
+import * as React from "react";
+import styled from "styled-components";
 
-import {faTrophy} from '@fortawesome/free-solid-svg-icons';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { faTrophy, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const TrophyIcon = styled(FontAwesomeIcon)`
     font-size: 1rem;
 `;
 
+const DeleteIcon = styled(FontAwesomeIcon)`
+    font-size: 0.8rem;
+    transition: color 0.2s;
+
+    &:hover {
+        cursor: pointer;
+        color: #317eac;
+    }
+`;
+
 interface IProps {
-    id: object;
-    text: string;
-    onAchivDelete: (id: object) => void;
+  id: object;
+  text: string;
+  onAchivDelete: (id: object) => void;
 }
 
 const Achievement: React.FC<IProps> = ({ id, text, onAchivDelete }) => (
-  <div className="lead d-flex">
-    <div className="d-flex-column align-items-center">
-      <TrophyIcon icon={faTrophy} />
-      <div></div>
+    <div className="lead d-flex">
+        <div className="d-flex-column align-items-center">
+            <TrophyIcon icon={faTrophy} />
+            <div></div>
+        </div>
+        <div className="pl-2">
+            {text}
+            <DeleteIcon
+                className="ml-2 align-middle"
+                icon={faTimes}
+                onClick={() => onAchivDelete(id)}
+            />
+        </div>
     </div>
-    <div className="pl-1">{text}</div>
-    <div className="">
-      <a className="pl-1" href="#" onClick={() => onAchivDelete(id)}>
-        delete
-      </a>
-    </div>
-  </div>
 );
 
-export default Achievement
+export default Achievement;
