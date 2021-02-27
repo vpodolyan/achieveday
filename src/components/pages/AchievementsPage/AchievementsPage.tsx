d5830274b0cabd381b1b8ab2ab96f4b16f02e488
Vladimir Podolyan <vpodolyan@outlook.com>
2021-02-27 21:25:04 +0300

    Refactor AchievementPage component

--- src/components/pages/AchievementsPage/AchievementsPage.tsx
+++ src/components/pages/AchievementsPage/AchievementsPage.tsx
@@ -1,18 +1,14 @@
-import { createContext, PureComponent } from 'react';
-import { connect } from 'react-redux';
-import { setUser } from 'actions';
-import stitchClient from 'stitch/client';
+import {
+  AchievementsForSelectedDate
+} from 'components/AchievementsForSelectedDate/AchievementsForSelectedDate';
+import { DatePickerContainer } from 'components/DatePicker/DatePickerContainer';
 import HeaderBar from 'components/HeaderBar';
 import MainContainer from 'components/MainContainer';
-import { DatePickerContainer } from 'components/DatePicker/DatePickerContainer';
-import { AchievementsForSelectedDate } from 'components/AchievementsForSelectedDate/AchievementsForSelectedDate';
 import { NewAchievement } from 'components/NewAchievement/NewAchievement';
-import StichAuthService from 'services/auth/StitchAuthService';
 import { QuoteOfDayController } from 'components/QuoteOfDayController/QuoteOfDayController';
-
-interface IProps {
-  setUser: typeof setUser;
-}
+import { createContext, FC } from 'react';
+import StichAuthService from 'services/auth/StitchAuthService';
+import stitchClient from 'stitch/client';
 
 const authService = new StichAuthService(stitchClient);
 
@@ -22,20 +18,16 @@ const authContexValue = {
 
 export const AuthContext = createContext(authContexValue);
 
-class AchievementsPage extends PureComponent<IProps> {
-  render () {
-    return (
-      <AuthContext.Provider value={authContexValue}>
-        <HeaderBar />
-        <MainContainer>
-          <DatePickerContainer />
-          <AchievementsForSelectedDate />
-          <NewAchievement />
-          <QuoteOfDayController />
-        </MainContainer>
-      </AuthContext.Provider>
-    );
-  }
-}
-
-export default connect(null, { setUser })(AchievementsPage);
+export const AchievementsPage: FC = () => {
+  return (
+    <AuthContext.Provider value={authContexValue}>
+      <HeaderBar />
+      <MainContainer>
+        <DatePickerContainer />
+        <AchievementsForSelectedDate />
+        <NewAchievement />
+        <QuoteOfDayController />
+      </MainContainer>
+    </AuthContext.Provider>
+  );
+};
