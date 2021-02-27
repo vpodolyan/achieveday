d758cefb9599b620b13ea945eeb43036f3f6ab80
Vladimir Podolyan <vpodolyan@outlook.com>
2021-02-27 21:16:55 +0300

    Refactor achievements container

---
+++ src/components/AchievementsForSelectedDate/AchievementsForSelectedDate.tsx
@@ -0,0 +1,45 @@
+import { getAchievements as getAchievementsAction, getAchievementsSuccess, removeAchievement, updateAchievement } from 'actions';
+import { AchievementsList } from 'components/AchievementsList/AchievementsList';
+import Spinner from 'components/Spinner/Spinner';
+import { FC, useCallback, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import achievementService from 'services/data/achievements/achievementsService';
+import IAchievement from 'types/IAchievement';
+import IAppState from 'types/state/IAppState';
+
+export const AchievementsForSelectedDate: FC = () => {
+  const loading = useSelector<IAppState>((state) => state.achievements.loading);
+  const selectedDate = useSelector<IAppState>((state) => state.datePicker.value);
+  const achievements = useSelector<IAppState, IAchievement[]>((state) => state.achievements.data);
+
+  const dispatch = useDispatch();
+
+  const handleAchievementDelete = (id) => {
+    dispatch(removeAchievement(id));
+  };
+
+  const getAchievements = useCallback(async (date) => {
+    dispatch(getAchievementsAction());
+    const achievements = await achievementService.getAchievements(date || new Date());
+    dispatch(getAchievementsSuccess(achievements));
+  }, [dispatch, getAchievementsAction, getAchievementsSuccess]);
+
+  const handleAchievementEdit = (achievement: IAchievement) => {
+    dispatch(updateAchievement(achievement));
+  };
+
+  useEffect(() => {
+    getAchievements(selectedDate);
+  }, [getAchievements, selectedDate]);
+
+  return (
+    <div className="d-flex justify-content-center">
+      {loading ? (
+        <div className="pt-5">
+          <Spinner size={2} />
+        </div>
+      ) : (
+        <AchievementsList achievements={achievements} onAchievementDelete={handleAchievementDelete} onAchievementEdit={handleAchievementEdit} />
+      )}
+    </div>);
+};
--- src/components/AchievementsList/AchievementsList.tsx
+++ src/components/AchievementsList/AchievementsList.tsx
@@ -1,52 +1,38 @@
-import { useEffect, FC } from 'react';
+import { FC } from 'react';
 import styled from 'styled-components';
 
 import IAchievement from 'types/IAchievement';
 import Achievement from '../Achievement/Achievement';
-import Spinner from '../Spinner/Spinner';
 
 const List = styled.div`
-    line-height: 2rem;
+  line-height: 2rem;
 `;
 
 interface IProps {
-    achievements: IAchievement[];
-    selectedDate: Date;
-    loading: boolean;
-    onAchievementDelete: (id: object) => void;
-    getAchievements: (date?: Date) => void;
-    onAchievementEdit: (achievement: IAchievement) => void;
+  achievements: IAchievement[];
+  onAchievementDelete: (id: object) => void;
+  onAchievementEdit: (achievement: IAchievement) => void;
 }
 
 export const AchievementsList: FC<IProps> = ({
-  achievements, onAchievementDelete, getAchievements, onAchievementEdit, selectedDate, loading
+  achievements,
+  onAchievementDelete,
+  onAchievementEdit
 }) => {
-  useEffect(
-    () => {
-      getAchievements(selectedDate);
-    }, [selectedDate]
-  );
-
   return (
-    <div className="d-flex justify-content-center">
-      {loading ? (
-        <div className="pt-5">
-          <Spinner size={2} />
-        </div>
-      ) : (
-        <List className="p-0 pt-4 pt-md-5 d-flex-column col-sm-12 col-md-8">
-          {achievements &&
-              achievements.map((achievement) => (
-                <Achievement
-                  key={achievement._id?.toString()}
-                  id={achievement._id}
-                  text={achievement.text}
-                  onAchivDelete={onAchievementDelete}
-                  onApplyChanges={(text) => onAchievementEdit({ ...achievement, text })}
-                />
-              ))}
-        </List>
-      )}
-    </div>
+    <List className="p-0 pt-4 pt-md-5 d-flex-column col-sm-12 col-md-8">
+      {achievements &&
+        achievements.map((achievement) => (
+          <Achievement
+            key={achievement._id?.toString()}
+            id={achievement._id}
+            text={achievement.text}
+            onAchivDelete={onAchievementDelete}
+            onApplyChanges={(text) =>
+              onAchievementEdit({ ...achievement, text })
+            }
+          />
+        ))}
+    </List>
   );
 };
