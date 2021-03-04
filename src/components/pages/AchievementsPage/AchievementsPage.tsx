a7003541a15b127c5c439564e0ae8c9b9c09a82a
Vladimir Podolyan <vpodolyan@outlook.com>
2021-03-04 14:09:22 +0300

    Refactoring HeaderBar component (#40)

--- src/components/pages/AchievementsPage/AchievementsPage.tsx
+++ src/components/pages/AchievementsPage/AchievementsPage.tsx
@@ -1,6 +1,6 @@
 import { AchievementsForSelectedDate } from 'components/AchievementsForSelectedDate/AchievementsForSelectedDate';
 import { DatePickerContainer } from 'components/DatePicker/DatePickerContainer';
-import HeaderBar from 'components/HeaderBar';
+import { HeaderBar } from 'components/HeaderBar/HeaderBar';
 import MainContainer from 'components/MainContainer';
 import { NewAchievement } from 'components/NewAchievement/NewAchievement';
 import { QuoteOfDayController } from 'components/QuoteOfDayController/QuoteOfDayController';
