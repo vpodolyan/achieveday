774bd2906397747b8342a6e3d5521a1a498f7797
Vladimir Podolyan <vpodolyan@outlook.com>
2021-03-04 13:59:09 +0300

    Refactoring HeaderBar component

--- src/components/HeaderBar/HeaderBar.tsx
+++ src/components/HeaderBar/HeaderBar.tsx
@@ -1,58 +1,33 @@
-import { PureComponent } from 'react';
-import * as React from 'react';
-import { WithTranslation, withTranslation } from 'react-i18next';
-import { connect } from 'react-redux';
-import { IAuthService, OAuthProviders } from 'services/auth/IAuthService';
+import { AuthContext } from 'components/App';
+import { FC, useContext } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useSelector } from 'react-redux';
 import IUser from 'types/IUser';
 import IAppState from 'types/state/IAppState';
 
 import { navigate } from '@reach/router';
 
-import { AuthContext } from '../App';
 import HeaderBarContainer from './HeaderBarContainer';
 
-interface IProps extends WithTranslation {
-  children?: React.ReactNode;
-  user: IUser;
-}
+export const HeaderBar: FC = ({ children }) => {
+  const { t } = useTranslation();
+  const user = useSelector<IAppState, IUser>((state) => state.user);
+  const { authService } = useContext(AuthContext);
 
-class HeaderBar extends PureComponent<IProps> {
-  render() {
-    const { t } = this.props;
-
-    return (
-      <HeaderBarContainer>
-        <AuthContext.Consumer>
-          {({ authService }: { authService: IAuthService }) =>
-            !authService.isAuthenticated() ? (
-              <div>
-                <button
-                  onClick={() => authService.logInOauth(OAuthProviders.Google)}
-                >
-                  {t('log_in')}
-                </button>
-              </div>
-            ) : (
-              <div>
-                <span className="pr-2">{this.props.user.name}</span>
-                <button
-                  onClick={() => {
-                    authService.logOut();
-                    navigate('/');
-                  }}
-                >
-                  {t('log_out')}
-                </button>
-              </div>
-            )
-          }
-        </AuthContext.Consumer>
-        {this.props.children}
-      </HeaderBarContainer>
-    );
-  }
-}
-
-export default connect((state: IAppState) => ({
-  user: state.user
-}))(withTranslation('common')(HeaderBar));
+  return (
+    <HeaderBarContainer>
+      <div>
+        <span className="pr-2">{user.name}</span>
+        <button
+          onClick={() => {
+            authService.logOut();
+            navigate('/');
+          }}
+        >
+          {t('log_out')}
+        </button>
+      </div>
+      {children}
+    </HeaderBarContainer>
+  );
+};
