c35c121b54672ef5b1a8f359ce39586bf75cec94
Vladimir Podolyan <vpodolyan@outlook.com>
2021-03-09 16:17:30 +0300

    Remove redundant HMR plugin usage

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,16 +1,13 @@
 const { merge } = require('webpack-merge');
-const webpack = require('webpack');
 const common = require('./webpack.common.js');
 
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin;
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'eval-cheap-module-source-map',
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new BundleAnalyzerPlugin({ analyzerMode: 'disabled' })
-  ],
+  plugins: [new BundleAnalyzerPlugin({ analyzerMode: 'disabled' })],
   devServer: {
     contentBase: './',
     open: true,
