9b5c6a9764a18e80b76f2a89a4fa23ce3033c7b2
Vladimir Podolyan <vpodolyan@outlook.com>
2021-04-02 17:55:54 +0300

    Introduce theming (#49)

---
+++ src/themes/lightTheme.ts
@@ -0,0 +1,11 @@
+import { ITheme } from 'types/ITheme';
+
+export const lightTheme: ITheme = {
+  id: '1',
+  name: 'light',
+  colors: {
+    loginPage: {
+      background: '#63c8f8'
+    }
+  }
+};
