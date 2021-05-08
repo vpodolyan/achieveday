66cd6b9aa931a914879fcee2b44a7effecd17ecc
Vladimir Podolyan <vpodolyan@outlook.com>
2021-05-08 19:34:00 +0300

    Use quotable as quotes API

--- src/middlewares/quotesMiddleware.ts
+++ src/middlewares/quotesMiddleware.ts
@@ -1,13 +1,13 @@
-import { Store } from 'redux';
+import { getDaiylyQuoteFailAction, getDaiylyQuoteSuccessAction } from 'actions';
 import { GET_DAILY_QUOTE } from 'actions/types';
-import { quotesService } from 'services/quotes/quotesService';
-import { getDaiylyQuoteSuccessAction, getDaiylyQuoteFailAction } from 'actions';
+import { Store } from 'redux';
+import { quotableService } from 'services/quotes/quotableService';
 
 export const quotesMiddleware = (store: Store) => (next) => async (action) => {
   const nextAction = next(action);
 
   if (action.type === GET_DAILY_QUOTE) {
-    const quote = await quotesService.getDailyQuote('inspire');
+    const quote = await quotableService.getDailyQuote();
 
     if (!quote) {
       store.dispatch(getDaiylyQuoteFailAction());
