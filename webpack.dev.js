b57e59121c3279ad19342e4002bbb8fc33a6d5dc
Vladimir Podolyan <vpodolyan@outlook.com>
2021-05-13 17:21:43 +0300

    Set up React Fast Refresh to get back HMR

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,19 +1,43 @@
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const ReactRefreshTypeScript = require('react-refresh-typescript');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
-  .BundleAnalyzerPlugin;
+const BundleAnalyzerPlugin =
+  require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'eval-cheap-module-source-map',
-  plugins: [new BundleAnalyzerPlugin({ analyzerMode: 'disabled' })],
+  plugins: [
+    new BundleAnalyzerPlugin({ analyzerMode: 'disabled' }),
+    new ReactRefreshWebpackPlugin()
+  ],
   devServer: {
     contentBase: './',
     open: true,
     compress: true,
     hot: true,
+    hotOnly: true,
     port: 4000,
     historyApiFallback: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(j|t)sx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              getCustomTransformers: () => ({
+                before: [ReactRefreshTypeScript()]
+              })
+            }
+          }
+        ]
+      }
+    ]
   }
 });
