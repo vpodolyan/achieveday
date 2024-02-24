4e622a35d8bc95fca2ca2fe0518280bec4ada878
Vladimir Podolyan <vpodolyan@outlook.com>
2024-02-24 13:28:39 +0300

    feat: quotes middleware replaced with react-query

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,15 @@
 import './utils/i18n';
 
-import { quotesMiddleware } from 'middlewares/quotesMiddleware';
-import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { applyMiddleware, createStore } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { createStore } from 'redux';
 import { ThemeManagementProvider } from 'theming/ThemeManagementProvider';
 
 import { App } from './components/App';
 import { reducers } from './reducers';
 
-const middlewares = applyMiddleware(quotesMiddleware);
-
-const store = createStore(
-  reducers,
-  process.env.NODE_ENV === 'development'
-    ? composeWithDevTools(middlewares)
-    : middlewares
-);
+const store = createStore(reducers);
 
 const queryClient = new QueryClient();
 
--- src/types/state/IQuotesState.ts
+++
@@ -1,7 +0,0 @@
-import { IQuote } from 'types/IQuote';
-
-export interface IQuotesState {
-  dailyQuote?: IQuote;
-  loading: boolean;
-  lastSuccessFetchDate: Date | undefined;
-}
