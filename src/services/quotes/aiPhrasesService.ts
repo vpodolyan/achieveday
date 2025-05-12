d403e0fbc58e4f58982b11665fb6f5c50cb142de
Vladimir Podolyan <vpodolyan@outlook.com>
2025-05-12 16:52:44 +0300

    fix: phrases style is too similar every day (#114)

--- src/services/quotes/aiPhrasesService.ts
+++ src/services/quotes/aiPhrasesService.ts
@@ -43,7 +43,7 @@ class AIPhrasesService implements IQuotesService {
 
           const userId = authService.getUserId() || '';
 
-          const phrase = await mistralService.completeMessages([
+          const message = await mistralService.completeMessages([
             {
               role: 'user',
               content: 'Hi! Please come up with some motivational phrase.'
@@ -54,30 +54,18 @@ class AIPhrasesService implements IQuotesService {
                 'Embrace each day as a chance to become a better version of yourself.'
             },
             {
-              role: 'assistant',
-              content:
-                'Embrace the storm within, for it is the wind that shapes the tallest trees.'
-            },
-            {
-              role: 'assistant',
-              content:
-                'Cultivate the garden of your mind, for even the smallest seed can grow into a mighty oak when nurtured by patience and persistence.'
-            },
-            {
-              role: 'assistant',
-              content:
-                "Unleash the dormant potential nestled deep within like a seed waiting for spring's first thaw, and watch your spirit bloom against all odds."
-            },
-            {
               role: 'user',
               content:
-                'Compose a unique motivational phrase focused on personal growth and self-improvement. It must be one or two sentences. Ensure the phrase starts with a different word each time, avoiding repetition. Incorporate a random element or metaphor, and use a varied sentence structure and vocabulary. Take inspiration from famous quotes. Explore different tones such as inspirational, reflective, or energetic. Do not use quotation marks. Display only one phrase'
+                'Compose an unique motivational phrase focused on personal growth and self-improvement. Ensure the phrase starts has different structure each time, avoiding repetition. Do not use any special symbols, except comma'
             }
           ]);
 
+          const phrases = message?.toString().split('\n\n');
+          const phrase = phrases?.[Math.floor(Math.random() * phrases.length)];
+
           const quote = {
             author: 'MistralAI',
-            text: phrase?.toString() || ''
+            text: phrase || ''
           };
 
           const newQuoteOfDay = {
